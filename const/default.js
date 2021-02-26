d6dff6cd874045ac82e12b4745893b239d9e1304
Igor Volodin <vinoron@yandex.ru>
2021-02-26 11:20:39 +0300

    clean master

--- const/default.js
+++
@@ -1,6 +0,0 @@
-export const GAMES_COLLECTION = 'games'
-export const ROUNDS_COLLECTION = 'rounds'
-export const PLAYERS_COLLECTION = 'players'
-
-export const SIDEBAR_KEY = 'LeftOpenDrawerSidebar'
-export const PAGE_LIMITS = [10, 20, 40]
